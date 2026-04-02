34d7b396eebd068e9cf50717138d64ad26c36401
Sujan Mishra <93533562+SujanMishra@users.noreply.github.com>
2026-04-02 12:34:41 -0400

    fix: add firebase subpath type declarations

---
+++ src/types/firebase-modules.d.ts
@@ -0,0 +1,11 @@
+declare module 'firebase/firestore' {
+  export * from '@firebase/firestore';
+}
+
+declare module 'firebase/storage' {
+  export * from '@firebase/storage';
+}
+
+declare module 'firebase/functions' {
+  export * from '@firebase/functions';
+}
