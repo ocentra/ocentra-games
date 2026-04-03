43bd00dae24c28b8810dbb73cd1d9355be4ea16e
Sujan Mishra <93533562+SujanMishra@users.noreply.github.com>
2026-04-03 18:15:11 -0400

    fix cloudflare helper exit code

--- infra/cloudflare/scripts/run-suite-helper.ts
+++ infra/cloudflare/scripts/run-suite-helper.ts
@@ -1189,7 +1189,7 @@ async function runForType(
   };
 }
 
-main().then((code) => process.exit(code)).catch((err) => {
+main().then((result) => process.exit(result.exitCode)).catch((err) => {
   console.error(err);
   process.exit(1);
 });
