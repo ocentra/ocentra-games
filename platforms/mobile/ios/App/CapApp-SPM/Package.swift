c2cc1c61495456bd659982fdf4b076f51fd26e72
Sujan Mishra <93533562+SujanMishra@users.noreply.github.com>
2026-05-10 13:06:24 -0400

    Fix Swift CodeQL package startup

--- platforms/mobile/ios/App/CapApp-SPM/Package.swift
+++ platforms/mobile/ios/App/CapApp-SPM/Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.1.0"),
-        .package(name: "CapacitorFirebaseAuthentication", path: "..\..\..\..\..\node_modules\@capacitor-firebase\authentication"),
-        .package(name: "CapacitorApp", path: "..\..\..\..\..\node_modules\@capacitor\app"),
-        .package(name: "CapacitorBrowser", path: "..\..\..\..\..\node_modules\@capacitor\browser"),
-        .package(name: "CapacitorPreferences", path: "..\..\..\..\..\node_modules\@capacitor\preferences"),
-        .package(name: "CapacitorSplashScreen", path: "..\..\..\..\..\node_modules\@capacitor\splash-screen"),
-        .package(name: "CapacitorStatusBar", path: "..\..\..\..\..\node_modules\@capacitor\status-bar")
+        .package(name: "CapacitorFirebaseAuthentication", path: "../../../../../node_modules/@capacitor-firebase/authentication"),
+        .package(name: "CapacitorApp", path: "../../../../../node_modules/@capacitor/app"),
+        .package(name: "CapacitorBrowser", path: "../../../../../node_modules/@capacitor/browser"),
+        .package(name: "CapacitorPreferences", path: "../../../../../node_modules/@capacitor/preferences"),
+        .package(name: "CapacitorSplashScreen", path: "../../../../../node_modules/@capacitor/splash-screen"),
+        .package(name: "CapacitorStatusBar", path: "../../../../../node_modules/@capacitor/status-bar")
     ],
     targets: [
         .target(
